1dd34d69aaedc2be7e4f9ad12865caf1b369adaa
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-02-09 19:13:58 -0300

    remove another vim.fn call

--- lua/vaquero-shoot/utils.lua
+++ lua/vaquero-shoot/utils.lua
@@ -1,48 +1,52 @@
 local M = {}
 
 M.toupleArrayElement = function(t)
-	local i = 0
+  local i = 0
 
-	return function()
-		i = i + 1
-		if t[i] then
-			return t[i][1], t[i][2]
-		end
-	end
+  return function()
+    i = i + 1
+    if t[i] then
+      return t[i][1], t[i][2]
+    end
+  end
 end
 
 M.getCol = function()
-	return vim.api.nvim_win_get_cursor(0)[2] + 1
+  return vim.api.nvim_win_get_cursor(0)[2] + 1
+end
+
+local function getLineNum()
+  return vim.api.nvim_win_get_cursor(0)[1]
 end
 
 local function execute(str)
-	vim.cmd(vim.api.nvim_replace_termcodes(str, true, true, true))
+  vim.cmd(vim.api.nvim_replace_termcodes(str, true, true, true))
 end
 
 M.getStartOfVisualSelection = function()
-	return vim.fn.getpos("v")[3]
+  return vim.fn.getpos("v")[3]
 end
 
 M.safePush = function(pile, element, i)
-	if pile[element] then
-		table.insert(pile[element], i)
-	else
-		pile[element] = { i }
-	end
+  if pile[element] then
+    table.insert(pile[element], i)
+  else
+    pile[element] = { i }
+  end
 end
 
 M.safePop = function(pile, element)
-	if pile[element] then
-		return table.remove(pile[element])
-	end
-	return nil
+  if pile[element] then
+    return table.remove(pile[element])
+  end
+  return nil
 end
 
 M.selectMoving = function(touple)
-	local lineNumber = vim.fn.line(".")
-	vim.fn.cursor(lineNumber, touple[1] + 1)
-	execute("normal<Esc>v")
-	vim.fn.cursor(lineNumber, touple[2] - 1)
+  local lineNumber = getLineNum()
+  vim.fn.cursor(lineNumber, touple[1] + 1)
+  execute("normal<Esc>v")
+  vim.fn.cursor(lineNumber, touple[2] - 1)
 end
 
 return M
